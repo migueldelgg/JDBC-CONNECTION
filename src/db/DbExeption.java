e0a60d6b508cd1162873318f115c957cecfe85ac
4Miguel2 <delgdevoleper@gmail.com>
2024-02-10 18:35:03 -0300

    conecção com banco jdbc

---
+++ src/db/DbExeption.java
@@ -0,0 +1,12 @@
+package db;
+
+import java.io.Serial;
+
+public class DbExeption extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DbExeption(String msg){
+        super(msg);
+    }
+}
